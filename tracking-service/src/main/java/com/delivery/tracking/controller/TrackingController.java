07c090c2d3f903034b603cc4fe8f683cb46c2c4b
Yurii Polishchuk <loc.dilp@gmail.com>
2026-09-21 13:56:39 +0300

    feat(tracknig): add tracking controller

---
+++ tracking-service/src/main/java/com/delivery/tracking/controller/TrackingController.java
@@ -0,0 +1,34 @@
+package com.delivery.tracking.controller;
+
+import com.delivery.tracking.entity.Location;
+import com.delivery.tracking.repository.TrackingRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tracking")
+public class TrackingController {
+
+    private final TrackingRepository trackingRepository;
+
+    public TrackingController(TrackingRepository trackingRepository) {
+        this.trackingRepository = trackingRepository;
+    }
+
+    @PostMapping
+    public Location createLocation(@RequestBody Location location) {
+        return trackingRepository.save(location);
+    }
+
+    @GetMapping
+    public List<Location> getLocations() {
+        return trackingRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Location getLocation(@PathVariable Long id) {
+        return trackingRepository.findById(id).orElseThrow();
+    }
+
+}
